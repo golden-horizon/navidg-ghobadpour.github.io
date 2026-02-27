d3f4d0c8053528b559c44c2517308c43b5e64100
Navid Ghobadpour <navid77g@gmail.com>
2026-02-27 15:40:20 +1100

    Add background color toggle functionality

---
+++ index.js
@@ -0,0 +1,11 @@
+const bodyEl = document.querySelector("body");
+const btn = document.getElementById("btn");
+
+let isBgColorGrey = true;
+
+function toggleBgColor() {
+  bodyEl.style.backgroundColor = isBgColorGrey ? "blue" : "grey";
+  isBgColorGrey = !isBgColorGrey;
+}
+
+btn.addEventListener("click", toggleBgColor);
